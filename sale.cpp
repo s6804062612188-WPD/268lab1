77b983f8e41640017974bbb188ba5cf60a5caa5b
Wongsathon Panthudech <s6804062612188@email.kmutnb.ac.th>
2025-12-23 23:25:33 +0700

    calculate VAT

--- sale.cpp
+++ sale.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>;
+#include <stdio.h>
 
 int main(){
 	float total, price; //the consecutive addition of prices, and total that is followed
@@ -6,12 +6,15 @@ int main(){
 	scanf("%f", &price);
 	total = price;
 	printf("%.2f\n", total*95/100); // one item
+	printf("vat7%%:\t%.2f\n", total*95/100*107/100); // one item, with 7% vat
 	
 	scanf("%f", &price);
 	total = total+price;
 	printf("%.2f\n", total*85/100); // NOW two items
+	printf("vat7%%:\t%.2f\n", total*85/100*107/100); // NOW two items, with 7% vat
 	
 	scanf("%f", &price);
 	total = total+price;
 	printf("%.2f\n", total*70/100); // NOW three items
+	printf("vat7%%:\t%.2f\n", total*70/100*107/100); // NOW three items, with 7% vat
 }
